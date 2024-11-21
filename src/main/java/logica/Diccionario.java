d3d7d868d0fe8002fd4a716c007ea1aefbeb96cb
Melqui Erazo <Melqui Erazo@LAPTOP-7DQNF4SS>
2024-11-20 22:07:04 -0500

    add proyect to repository

---
+++ src/main/java/logica/Diccionario.java
@@ -0,0 +1,16 @@
+package logica;
+import igu.Pantalla;
+/**
+ *
+ * @author Melqui Erazo
+ */
+public class Diccionario extends javax.swing.JFrame {
+
+    public static void main(String[] args) {
+        Pantalla pantalla = new Pantalla();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+        pantalla.ocultarLabel();
+
+    }
+}
